1b5812275d9895a60e48261895acfd5016514cb9
Subodh Kumar Singh <mailstosubodh@gmail.com>
2016-03-20 20:15:50 +0530

    Added CORS

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'devise'
 gem 'omniauth'
 gem 'rabl'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module Poise
     config.assets.paths << Rails.root.join('node_modules')
 
     config.autoload_paths << Rails.root.join('lib')
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
