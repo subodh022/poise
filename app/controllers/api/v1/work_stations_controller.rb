b5c78e2dbae58c850358fa2453aba499891e1e22
Subodh Kumar Singh <mailstosubodh@gmail.com>
2016-03-28 15:13:58 +0530

    workstation by ob id

--- app/controllers/api/v1/work_stations_controller.rb
+++ app/controllers/api/v1/work_stations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::WorkStationsController < ApiController
 
 	def index
-	  @work_stations = WorkStation.includes(:operation_bulletin, :section, :operation, :machine, :operator)
+	  @work_stations = WorkStation.includes(:operation_bulletin, :section, :operation, :machine, :operator).where("operation_bulletin_id = ?", params[:operation_bulletin_id])
 
 	  respond_to do |format|
 	   format.json{ @work_stations.to_json }
